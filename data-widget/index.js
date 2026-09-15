3cd44df587105c6caf02f6b56262393304afa38f
chebishev <atanas.chebishev@gmail.com>
2026-09-15 16:12:25 +0300

    add index.layout.js which will be the only layout file r and s version just extend it

--- data-widget/index.js
+++ data-widget/index.js
@@ -1,12 +1,29 @@
-DataWidget({
-  state: {},
+import {
+  createWidget,
+  widget,
+  keyboard,
+} from '@zos/ui'
 
+DataWidget({
   onInit() {
     console.log('BG keyboard: onInit')
   },
 
   build() {
     console.log('BG keyboard: build')
+
+    createWidget(widget.BUTTON, {
+      x: 200,
+      y: 250,
+      w: 80,
+      h: 60,
+      text: 'а',
+
+      click_func: () => {
+        console.log('Pressed: а')
+        keyboard.inputText('а')
+      },
+    })
   },
 
   onDestroy() {
---
+++ data-widget/index.layout.js
@@ -0,0 +1,10 @@
+import { px } from '@zos/utils'
+
+export const styles = {
+  firstRow: {
+    x: px(20),
+    y: px(220),
+    keyWidth: px(40),
+    keyHeight: px(60),
+  },
+}
